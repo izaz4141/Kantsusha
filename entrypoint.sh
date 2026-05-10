417022efb788b50d78f56985bc78a9812fd9a736
Glicole <izazwidyan@gmail.com>
2026-05-10 14:12:19 +0700

    fix: alpine specific secret generation

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@ set -e
 
 export KANTSUSHA_DATABASE_URL=${KANTSUSHA_DATABASE_URL:-./db/kantsusha.db}
 export KANTSUSHA_ORIGINS=${KANTSUSHA_DATABASE_URL:-http://localhost:*,http://127.0.0.1:*}
-export KANTSUSHA_AUTH_SECRET=${KANTSUSHA_DATABASE_URL:-$(openssl rand -base64 32)}
+export KANTSUSHA_AUTH_SECRET=${KANTSUSHA_AUTH_SECRET:-$(head -c 32 /dev/urandom | base64 | tr -d '\n')}
 
 mkdir db config
 
