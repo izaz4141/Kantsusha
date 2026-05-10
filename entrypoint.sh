781d1adb0a9e5fd393c15a38f4681f0259dfdc8f
Glicole <izazwidyan@gmail.com>
2026-05-10 18:05:18 +0700

    fix; ORIGINS env

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 set -e
 
 export KANTSUSHA_DATABASE_URL=${KANTSUSHA_DATABASE_URL:-./db/kantsusha.db}
-export KANTSUSHA_ORIGINS=${KANTSUSHA_DATABASE_URL:-http://localhost:*,http://127.0.0.1:*}
+export KANTSUSHA_ORIGINS=${KANTSUSHA_ORIGINS:-http://localhost:*,http://127.0.0.1:*}
 export KANTSUSHA_AUTH_SECRET=${KANTSUSHA_AUTH_SECRET:-$(head -c 32 /dev/urandom | base64 | tr -d '\n')}
 
 mkdir -p db config
